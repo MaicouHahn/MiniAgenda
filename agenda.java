3ec81cebdd7543bbe77c64e90d291e5e054ce505
Maicou Hahn Fortuna <111459606+MaicouHahn@users.noreply.github.com>
2022-11-05 17:24:22 -0300

    Add files via upload

--- agenda.java
+++ agenda.java
@@ -4,28 +4,34 @@ public class agenda {
 
     public Scanner scan = new Scanner(System.in);
     public String nome;
-    public String[] data = new String[7];
-    public String[] at = new String[7];
-    public String[] ds = new String[7];
+    public int qtde=7;
+    public String[] data = new String[qtde];
+    public String[] at = new String[qtde];
+    public String[] ds = new String[qtde];
+    public String[] hr = new String[qtde];
     public int numDIA = 7;
 
     public void getInfo() {
 
         for (int i = 0; i < numDIA; i++) {
 
-            System.out.println("Digite a data " + (i + 1));
-            data[i] = scan.nextLine();
-            System.out.println("Digite o dia da semana " + (i + 1));
+            System.out.println("Digite o dia da semana em que fara a atividade" + (i + 1));
             ds[i] = scan.nextLine();
-            System.out.println("Digite a atividade " + (i + 1));
+            System.out.println("Digite a data do dia dessa atividade" + (i + 1));
+            data[i] = scan.nextLine();
+            System.out.println("Que atividade voce fara nesse dia? ");
             at[i] = scan.nextLine();
+            System.out.println("Digite o horario do dia que fara essa atividade");
+            hr[i] = scan.nextLine();
+
         }
     }
 
-    public void impALL(String nome, String data, String ds, String at) {
+    public void impALL(String ds, String data, String hr, String at) {
 
-        System.out.printf("%s\n%s\n%s\n%s\n", nome, data, ds, at);
+        System.out.printf("\nDia: %s - %s\nHorario: %s\nAtividade: %s\n", ds, data, hr, at);
         System.out.println("========================");
+
     }
 
     public String Nome() {
@@ -33,6 +39,10 @@ public class agenda {
         System.out.println("Digite seu nome");
         nome = scan.nextLine();
         return nome;
+    }
+
+    public void NomeImp(String  nome){
+        System.out.print("\n========================\n" + nome + "\n========================");
     }
 
 }
--- main.java
+++ main.java
@@ -8,29 +8,41 @@ public class main {
         Scanner scanDT = new Scanner(System.in);
         String DT;
         agenda a1 = new agenda();
-        int bt;
+        int bt, bt2 = 0;
         a1.Nome();
         a1.getInfo();
 
         System.out.println("Como deseja consultar? 1-Toda Lista 2- Consulta individual por data");
         bt = scanBT.nextInt();
+        
         switch (bt) {
             case 1:
 
+                a1.NomeImp(a1.nome);
+
                 for (int i = 0; i < a1.numDIA; i++) {
-                    a1.impALL(a1.nome, a1.data[i], a1.ds[i], a1.at[i]);
+                    a1.impALL(a1.ds[i], a1.data[i], a1.hr[i], a1.at[i]);
                 }
                 break;
 
             case 2:
-                System.out.println("Informe a data");
-                DT = scanDT.nextLine();
 
-                for (int i = 0; i < a1.numDIA; i++) {
-                    if (DT.equals(a1.data[i])) {
-                        a1.impALL(a1.nome, a1.data[i], a1.ds[i], a1.at[i]);
-                    } else {
-                        System.out.println("data invalida");
+                while (bt2 == 0) {
+
+                    System.out.println("Informe a data");
+                    DT = scanDT.nextLine();
+
+                    for (int i = 0; i < a1.numDIA; i++) {
+
+                        if (DT.equals(a1.data[i])) {
+
+                            a1.NomeImp(a1.nome);
+
+                            a1.impALL(a1.ds[i], a1.data[i], a1.hr[i], a1.at[i]);
+                            bt2 = 1;
+                        } else {
+                            System.out.println("data invalida");
+                        }
                     }
                 }
 
