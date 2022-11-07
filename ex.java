aa46b74976209b77015af6b350b41c80ea543a6e
Maicou Hahn Fortuna <111459606+MaicouHahn@users.noreply.github.com>
2022-11-07 19:22:10 -0300

    Add files via upload

---
+++ ex.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ex {
+
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        String[] a=new String[]{"a","b","c"};
+        /* 
+        String nome[]=new String[5];
+        
+        for (int i = 0; i < nome.length ; i++) {
+
+            System.out.println("Nome: ");
+            nome[i]=scan.nextLine();
+            
+        }
+        */
+        Arrays.sort(a);
+    }
+
+}
